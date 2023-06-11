dca188a9a45d4a158f320dc606b1f31e83de6a51
HardBoris <bgaibor03@gmail.com>
2023-06-10 21:23:56 -0400

    feat: home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,24 @@
+// import "../../styles/layout.css";
+import "./style.css";
+export const Home = () => {
+  return (
+    <>
+      <header>
+        <h1>Home</h1>
+      </header>
+      <main>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+        <div className="prueba"></div>
+      </main>
+      <footer></footer>
+    </>
+  );
+};
