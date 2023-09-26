3208375e912037706674b64e69f275fffbd3d119
HardBoris <bgaibor03@gmail.com>
2023-09-26 02:09:35 +0000

    Probando replit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ export const Home = () => {
         <title>Aventura | Home</title>
       </Helmet>
       <div className="welcome">
-        <h1>Aventura</h1>
+        <h1>Oikos</h1>
         <h2>Controle de Estoque</h2>
       </div>
     </>
